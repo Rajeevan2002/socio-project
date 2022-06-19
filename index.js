05c396e7d786441e4392a61a37527f077eb9ace9
Rajeevan2002 <63632495+Rajeevan2002@users.noreply.github.com>
2022-06-19 11:13:50 +0530

    make it better

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import path from 'path'
 
 import postRoutes from './routes/posts.js'
 import userRoutes from './routes/users.js'
@@ -31,7 +30,10 @@ const PORT = process.env.PORT || 5000
 //     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 // }
 
-mongoose.connect(process.env.CONNECTION_URL || "mongodb://localhost/socio", ({
+
+
+
+mongoose.connect(process.env.CONNECTION_URL || "mongodb://localhost/rajeevan", ({
     useNewUrlParser: true, useUnifiedTopology: true
 }))
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
