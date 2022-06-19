73b71a697be2e4ac4e54054319583c73330763cb
Rajeevan2002 <63632495+Rajeevan2002@users.noreply.github.com>
2022-06-19 11:02:30 +0530

    make it better

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const PORT = process.env.PORT || 5000
 //     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 // }
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/socio", ({
+mongoose.connect(process.env.CONNECTION_URL || "mongodb://localhost/socio", ({
     useNewUrlParser: true, useUnifiedTopology: true
 }))
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
